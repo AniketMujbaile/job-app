7d64a52eaef3807d5f1e2fba7ddddf1fe2d94017
Aniket Mujbaile <81378709+AniketMujbaile@users.noreply.github.com>
2024-09-01 12:42:30 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // const API_URL = 'http://localhost:5000/api'; 
-const API_URL = 'https://job-app-1sfa.onrender.com/api'; 
+//const API_URL = 'https://job-app-1sfa.onrender.com/api'; 
+const API_URL = 'https://job-app-wioi.vercel.app/api'';
 
 const api = axios.create({
   baseURL: API_URL,
