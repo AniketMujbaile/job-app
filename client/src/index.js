682c2e1fb3e501c07fe00eb3bd2c00abf3f2205d
Aniket Mujbaile <81378709+AniketMujbaile@users.noreply.github.com>
2024-09-01 10:29:07 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import store, { persistor } from './store';
 import App from './App';
 import './index.css';
 
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);  
 
-ReactDOM.render(
+root.render(
   <Provider store={store}>
     <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
       <App />
     </PersistGate>
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/integration/react';
+// import store, { persistor } from './store';
+// import App from './App';
+// import './index.css';
+
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+//       <App />
+//     </PersistGate>
+//   </Provider>,
+//   document.getElementById('root')
+// );
 
 // import React from 'react';
 // import ReactDOM from 'react-dom/client';
